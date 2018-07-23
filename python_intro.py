e1bca91cb2919f5e787b0ce6df5c1a863579c69d
Rebecca <rebecca@floatapp.com>
2018-07-23 14:35:10 +0100

    My Django Girls app, first commit

---
+++ python_intro.py
@@ -0,0 +1,23 @@
+if 3 > 2:
+    print('It works!')
+if 5 > 2:
+    print('5 is indeed greater than 2')
+else:
+    print('5 is not greater than 2')
+#How do comments work
+#Oh I see
+def hi():
+    print('Hi there!')
+    print('How are you?')
+
+hi()
+def hi(name):
+    if name == 'Ola':
+        print('Hi Ola!')
+    elif name == 'Sonja':
+        print('Hi Sonja!')
+    else:
+        print('Hi anonymous')
+
+hi('name')
+
